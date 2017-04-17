3113b36f0f63bf9ece3dca9bcf31b488706c27c5
joe223 <heyin223@163.com>
2017-04-17 12:04:29 +0800

    rebuild

---
+++ lib2/index.js
@@ -0,0 +1,13 @@
+import load from "./utils/load.js";
+import map from "./components/map.vue";
+import mapMarker from "./components/mapMarker.vue";
+import mapPolyline from "./components/mapPolyline.vue";
+import mapInfoWindow from "./components/mapInfoWindow.vue";
+import controllerBox from "./components/controllerBox.vue";
+import zoomController from "./components/zoomController.vue";
+import toggleTypeController from "./components/toggleTypeController.vue";
+import fullScreenController from "./components/fullScreenController.vue";
+import localSearchController from "./components/localSearchController.vue";
+import locateController from "./components/locateController.vue";
+
+export { load, map, mapMarker, mapPolyline, mapInfoWindow, controllerBox, zoomController, toggleTypeController, fullScreenController, localSearchController, locateController };
---
+++ lib2/utils/bindEvent.js
@@ -0,0 +1,31 @@
+/**
+ * mapoverlay 添加事件处理
+ * @param $component    map组件所在的vue组件
+ * @param componentObj  map组件
+ * @param eventList     支持的事件名列表
+ */
+// export default ( $component, componentObj, eventList ) => {
+//     if ( eventList && eventList instanceof Array ) {
+//         eventList.map( function ( item, index, arr ) {
+//             componentObj.addEventListener( item, function ( e ) {
+//                 $component.$emit( "b-" + item, e, $component );
+//             })
+//         })
+//     } else {
+//         if ( process.env.NODE_ENV === "development" ) console.error( "[vue-baidu-map] param eventList must be an Array!" );
+//     }
+// }
+
+// rebuild
+export default function ( mapComponentObj, eventList ) {
+    if ( eventList && eventList instanceof Array ) {
+        eventList.map( ( item, index ) => {
+            mapComponentObj.addEventListener( item, ( e ) => {
+                this.$emit( "b-" + item, e, this );
+            })
+        })
+    } else {
+        if ( process.env.NODE_ENV === "development" )
+            console.error( "[vue-baidu-map] param eventList must be an Array!" );
+    }
+}
